2d302fb2c941d24941ce5267fd77c2150a956798
root23 <hipstor.pnz@gmail.com>
2021-02-21 23:27:58 +0300

    added blocks

---
+++ resources/views/components/sizes-block.blade.php
@@ -0,0 +1,51 @@
+<div class="sizes__container">
+    <h1 class="title sizes__title">Варианты размеров пазлов Unidragon</h1>
+</div>
+<div class="sizes__main">
+    <div class="sizes__container">
+        <div class="sizes__items">
+            <div class="c-size sizes__item">
+                <div class="c-size__header">
+                    <h3 class="c-size__title">Размер S (A5)</h3>
+                </div>
+                <div class="c-size__main">
+                    <p class="c-size__price">1 490 руб. <span class="c-size__price-unit">/ набор</span></p>
+                    <ul class="c-size__list">
+                        <li>Площадь: ~400 см2</li>
+                        <li>Деталей: ~100 шт.</li>
+                        <li>Время сборки: 1-2 часа</li>
+                        <li>Самостоятельная сборка 7+ лет</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="c-size sizes__item">
+                <div class="c-size__header">
+                    <h3 class="c-size__title">Размер M (A4)</h3><span class="c-size__ribbon">2x больше S</span>
+                </div>
+                <div class="c-size__main">
+                    <p class="c-size__price">2 490 руб. <span class="c-size__price-unit">/ набор</span></p>
+                    <ul class="c-size__list">
+                        <li>Площадь: ~800 см2</li>
+                        <li>Деталей: ~190 шт.</li>
+                        <li>Время сборки: 2-4 часа</li>
+                        <li>Самостоятельная сборка 10+ лет</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="c-size sizes__item">
+                <div class="c-size__header">
+                    <h3 class="c-size__title">Размер King Size (A3)</h3><span class="c-size__ribbon">3x больше S</span>
+                </div>
+                <div class="c-size__main">
+                    <p class="c-size__price">3 490 руб. <span class="c-size__price-unit">/ набор</span></p>
+                    <ul class="c-size__list">
+                        <li>Площадь: ~1200 см2</li>
+                        <li>Деталей: ~325 шт.</li>
+                        <li>Время сборки: 4-6 часов</li>
+                        <li>Самостоятельная сборка 14+ лет</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
