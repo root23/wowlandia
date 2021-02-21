301e860896b59e6d2f2bc6d006a12dcc23af5fb4
root23 <hipstor.pnz@gmail.com>
2021-02-21 23:13:33 +0300

    add header block

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,55 @@
+<div class="header__container"><button class="btn-menu header__btn-menu" type="button" title="Меню"><span class="btn-menu__in"></span></button>
+    <div class="logo logo--header header__logo">
+        <img src="https://unidragon.ru/image/catalog/general/logo@mob.png?v=2" width="80" height="50" alt="unidragon.ru" />
+        <img src="https://unidragon.ru/image/catalog/Logo-unidragon.png?v=2" width="223" height="61" alt="unidragon.ru" />
+    </div>
+
+
+    <a class="btn-cart js-popup header__btn-cart" href="#cart-popup" role="button" title="Корзина">
+        <svg class="svg-icon svg-icon--cart">
+            <use xlink:href="#cart"></use>
+        </svg><span class="btn-cart__quantity">2</span>
+    </a>
+
+    <div class="header__nav">
+        <nav class="menu header__menu">
+            <ul class="menu__items">
+                <li class="menu__item"><a class="js-scrollto" href="#products">Деревянные пазлы</a></li>
+                <li class="menu__item"><a class="js-scrollto" href="#videos">Видео</a></li>
+                <li class="menu__item"><a class="js-scrollto" href="#faq">Faq</a></li>
+                <li class="menu__item"><a class="js-scrollto" href="#testimonials">Отзывы</a></li>
+                <li class="menu__item"><a class="js-scrollto" href="#delivery">Доставка и оплата</a></li>
+            </ul>
+        </nav>
+        <a class="email email--header header__email" href="mailto:care@unidragon.com">
+            <svg class="svg-icon svg-icon--email email__icon">
+                <use xlink:href="#email"></use>
+            </svg>
+            <span class="email__label">E-mail:</span>
+            <div class="email__email">care@unidragon.com</div>
+        </a>
+        <a class="phone phone--header header__phone" href="tel:88007775762"><svg class="svg-icon svg-icon--phone phone__icon">
+                <use xlink:href="#phone"></use>
+            </svg><span class="phone__label">Телефон:</span>
+            <div class="phone__phone">8 800 777 57 62</div>
+        </a>
+        <div class="social social--header header__social">
+            <div class="social__buttons">
+                <a class="social__button" href="" role="button" target="_blank" rel="nofollow noopener">
+                    <svg class="svg-icon svg-icon--social-vk">
+                        <use xlink:href="#social-vk"></use>
+                    </svg>
+                </a>
+
+                <a class="social__button" href="" role="button" target="_blank" rel="nofollow noopener">
+                    <svg class="svg-icon svg-icon--social-instagram">
+                        <use xlink:href="#social-instagram"></use>
+                    </svg>
+                </a>
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>unidragon.ru</title>
+    <title>wowlandia.ru</title>
 
     <link rel="stylesheet" href="font/stylesheet.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
@@ -43,62 +43,9 @@
 
 
 <header class="header page__header">
-    <div class="header__container"><button class="btn-menu header__btn-menu" type="button" title="Меню"><span class="btn-menu__in"></span></button>
-        <div class="logo logo--header header__logo">
-            <img src="https://unidragon.ru/image/catalog/general/logo@mob.png?v=2" width="80" height="50" alt="unidragon.ru" />
-            <img src="https://unidragon.ru/image/catalog/Logo-unidragon.png?v=2" width="223" height="61" alt="unidragon.ru" />
-        </div>
-
-
-        <a class="btn-cart js-popup header__btn-cart" href="#cart-popup" role="button" title="Корзина">
-            <svg class="svg-icon svg-icon--cart">
-                <use xlink:href="#cart"></use>
-            </svg><span class="btn-cart__quantity">2</span>
-        </a>
-
-        <div class="header__nav">
-            <nav class="menu header__menu">
-                <ul class="menu__items">
-                    <li class="menu__item"><a class="js-scrollto" href="#products">Деревянные пазлы</a></li>
-                    <li class="menu__item"><a class="js-scrollto" href="#videos">Видео</a></li>
-                    <li class="menu__item"><a class="js-scrollto" href="#faq">Faq</a></li>
-                    <li class="menu__item"><a class="js-scrollto" href="#testimonials">Отзывы</a></li>
-                    <li class="menu__item"><a class="js-scrollto" href="#delivery">Доставка и оплата</a></li>
-                </ul>
-            </nav>
-            <a class="email email--header header__email" href="mailto:care@unidragon.com">
-                <svg class="svg-icon svg-icon--email email__icon">
-                    <use xlink:href="#email"></use>
-                </svg>
-                <span class="email__label">E-mail:</span>
-                <div class="email__email">care@unidragon.com</div>
-            </a>
-            <a class="phone phone--header header__phone" href="tel:88007775762"><svg class="svg-icon svg-icon--phone phone__icon">
-                    <use xlink:href="#phone"></use>
-                </svg><span class="phone__label">Телефон:</span>
-                <div class="phone__phone">8 800 777 57 62</div>
-            </a>
-            <div class="social social--header header__social">
-                <div class="social__buttons">
-                    <a class="social__button" href="" role="button" target="_blank" rel="nofollow noopener">
-                        <svg class="svg-icon svg-icon--social-vk">
-                            <use xlink:href="#social-vk"></use>
-                        </svg>
-                    </a>
-
-                    <a class="social__button" href="" role="button" target="_blank" rel="nofollow noopener">
-                        <svg class="svg-icon svg-icon--social-instagram">
-                            <use xlink:href="#social-instagram"></use>
-                        </svg>
-                    </a>
-
-
-
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('components.header')
 </header>
+
 <section class="first page__first" id="first">
     <div class="first__container">
         <h1 class="first__title">Увлекательные деревянные пазлы</h1>
@@ -127,8 +74,6 @@
         </div>
     </div>
 </section>
-
-
 
 <section class="sizes page__sizes" id="sizes">
     <div class="sizes__container">
@@ -379,7 +324,7 @@
     </div>
     <div class="footer__bottom">
         <div class="footer__container">
-            <p class="footer__info">ИП Ружейников СН, ИНН 631629959173443011, г Самара, ул Ново-садовая 303А оф. 23, ОГРНИП 318631300132807 = ООО "ВЕРБА», ИНН: 7723425777, г. Москва,  ул. Совхозная, дом 10, офис 21
+            <p class="footer__info">ООО "ВЕРБА», ИНН: 7723425777, г. Москва,  ул. Совхозная, дом 10, офис 21
             </p>
         </div>
     </div>
