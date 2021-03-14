8f346feac9b364d3c783e8df40b8f81983711cad
root23 <hipstor.pnz@gmail.com>
2021-03-14 15:44:57 +0300

    variable fix

--- resources/views/mail/order-mail.blade.php
+++ resources/views/mail/order-mail.blade.php
@@ -1,4 +1,4 @@
 <h1>Уведомление о новом заказе</h1>
 
 <p>Был оформлен новый заказ!</p>
-<p><a href="https://wowlandia.ru/admin/order/{{ $data->orderId }}">Открыть данные заказа</a> </p>
+<p><a href="https://wowlandia.ru/admin/order/{{ $mailData->orderId }}">Открыть данные заказа</a> </p>
