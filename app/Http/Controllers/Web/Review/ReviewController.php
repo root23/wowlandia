9135740c386e6f8c39fe63ecdee38f7d46520151
root23 <hipstor.pnz@gmail.com>
2021-03-17 22:42:52 +0300

    fix reviews

--- app/Http/Controllers/Web/Review/ReviewController.php
+++ app/Http/Controllers/Web/Review/ReviewController.php
@@ -21,14 +21,18 @@ class ReviewController extends Controller {
         if ($request->hasFile('file')) {
             if ($request->file('file')->isValid()) {
                 $validated = $request->validate([
-                   'name' => 'string|max:255',
-                   'image' => 'mimes:jpeg,png|max:1014',
+                    'name' => 'string|max:255',
+                    'image' => 'mimes:jpeg,png|max:1014',
                 ]);
+
                 $extension = $request->file->extension();
                 $filename = uniqid();
                 $request->file->storeAs('/public/reviews/', $filename . "." . $extension);
                 $url = Storage::url('reviews/' . $filename.".".$extension);
                 $review->image = $url;
+            }
+        }
+
                 $review->is_active = 0;
                 $review->product_id = $request->get('product_id');
                 $review->email = $request->get('email');
@@ -37,8 +41,6 @@ class ReviewController extends Controller {
                 $review->title = $request->get('name');
                 $review->save();
                 Session::flash('success', "Success!");
-            }
-        }
 
         return redirect('/')->with('status', 'review-added');
 
