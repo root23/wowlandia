6913e8501ab22fbb554349d2382457f56f73679e
root23 <hipstor.pnz@gmail.com>
2021-03-27 23:12:31 +0300

    add image encoding to jpg

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use Intervention\Image\Facades\Image;
+
+class ProductObserver
+{
+    public function created(Product $product)
+    {
+        $this->MakeJpgImage($product);
+    }
+
+    public function updated(Product $product)
+    {
+        $this->MakeJpgImage($product);
+    }
+
+    private function MakeJpgImage(Product $product)
+    {
+        if (pathinfo($product->cover_image)['extension'] != 'jpg') {
+            $imageName = pathinfo($product->cover_image)['basename'] . '.jpg';
+            $savePath = storage_path() . '/app/public/' . date('Y') . '/' . date('m') . '/' . date('d') . '/' . $imageName;
+            $newCoverImage = '/storage/' . date('Y') . '/' . date('m') . '/' . date('d') . '/' . $imageName;
+            $image = Image::make(env('APP_URL') . '/' . $product->cover_image)->encode('jpg', 75)->save($savePath);
+            $product->cover_image = $newCoverImage;
+            $product->save();
+        }
+    }
+}
